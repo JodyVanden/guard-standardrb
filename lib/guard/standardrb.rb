2865202a4ee4b55e7132c1fae429337aa8d55941
Jody Vandenschrick <jody.vandenschrick@cultureamp.com>
2020-04-08 14:58:22 +1000

    Initializisation for the gem guard-standardrb

---
+++ lib/guard/standardrb.rb
@@ -0,0 +1,8 @@
+require "guard/standardrb/version"
+
+module Guard
+  module Standardrb
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/guard/standardrb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Guard::Standardrb do
+  it "has a version number" do
+    expect(Guard::Standardrb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
