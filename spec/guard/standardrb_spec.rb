6e2e67605a7be3d38186a5ea78ef8637483cfb02
Jody Vandenschrick <jody.vanden@gmail.com>
2020-07-05 22:52:37 +1000

    Add tests for the standardrb class

--- spec/guard/standardrb_spec.rb
+++ spec/guard/standardrb_spec.rb
@@ -1,9 +1,36 @@
+require "guard/compat/test/helper"
+
 RSpec.describe Guard::Standardrb do
   it "has a version number" do
-    expect(Guard::Standardrb::VERSION).not_to be nil
+    expect(Guard::StandardrbVersion).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:standardrb) {Guard::Standardrb.new}
+
+  describe "#start" do
+    it "prints message" do
+      expect(Guard::Compat::UI).to receive(:info).
+        with("Inspecting Ruby code style of all files with standardrb")
+
+        standardrb.start
+    end
+  end
+
+  describe "#run_on_modifications" do
+    it "prints message" do
+      expect(Guard::Compat::UI).to receive(:info).
+        with("StandardRb a file was modified")
+
+        standardrb.run_on_modifications([])
+    end
+  end
+
+  describe "#inspect_with_standardrb" do
+    let(:paths) {["lorem", "lorem"]}
+
+    it "runs standardrb on the files" do
+      allow_any_instance_of(Kernel).to receive(:system)
+      expect(standardrb.inspect_with_standardrb(paths)).to eq(paths)
+    end
   end
 end
