887891de75d78319d1f2961e40fbbb3c38077da5
eljetico <tim@eljetico.co.uk>
2021-03-17 22:13:48 +0000

    Add option to run all on start
    Enable guard-standardrb to be configured with `:all_on_start` option, which triggers
    `run_all` on startup. Default value is false.
    
    Found some issues with uninitialized constant 'Guard::Compat' when testing, fixed by
    amending the references to 'Guard::UI' as appropriate.
    
    Also fixed some infractions to standardrb rules throughout.

--- lib/guard/standardrb.rb
+++ lib/guard/standardrb.rb
@@ -1,23 +1,29 @@
-require "guard"
 require "guard/plugin"
 require "guard/standardrb/version"
 
 module Guard
   class Standardrb < Plugin
-    attr_reader :fix
-
     def initialize(opts = {})
       super
-      @fix = opts[:fix]
+
+      @options = {
+        fix: false,
+        all_on_start: false
+      }.merge(opts)
     end
 
     def start
-      Guard::Compat::UI.info "Inspecting Ruby code style of all files with standardrb"
-      Guard::Compat::UI.info "Standardrb --fix = #{fix}"
+      Guard::UI.info "Inspecting Ruby code style of all files with standardrb"
+      Guard::UI.info "Standardrb --fix = #{@options[:fix]}"
+      run_all if @options[:all_on_start]
+    end
+
+    def run_all
+      inspect_with_standardrb
     end
 
     def run_on_modifications(res)
-      Guard::Compat::UI.info "StandardRb a file was modified"
+      Guard::UI.info "StandardRb a file was modified"
       inspect_with_standardrb(res)
 
       $stdout.puts res if res
@@ -25,7 +31,7 @@ module Guard
 
     def inspect_with_standardrb(paths = [])
       args = ["bundle", "exec", "standardrb"]
-      args << "--fix" if fix
+      args << "--fix" if @options[:fix]
 
       args += paths
       system(*args)
